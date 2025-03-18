6c8ddaeb33a71fffe92c65e59c65313a785de87c
Henry <henry_antunes@estudante.sesisenai.org.br>
2025-03-18 10:10:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log('hello, world')
+let nome = "henry";
+
+function saudacao() {
+    console.log("ola, " + nome);
+
+}
+
+
+    function verificarPar(numero) {
+    if (numero % 2 === 0)  {
+
+    console.log("par")
+    } else {
+    console.log("impar");
+    }
+}
+
+
+
+
+
+
+    
